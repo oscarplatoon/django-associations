8e81db00983c4e2ee984a0bd322638d7f4f99287
Won Yang <won@gatech.edu>
2021-06-25 09:59:54 -0500

    adds medium solution

--- medium/blog/models.py
+++ medium/blog/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
 class User(models.Model):
     pass
 
+
 class Post(models.Model):
-    pass
+    # a post has one author. link a foreignkey to author
+    author = models.ForeignKey(
+        'User', on_delete=models.CASCADE, related_name='posts')
+
 
 class Comment(models.Model):
-    pass
-  
+    # a comment only belongs to one post. link a foreignkey to post
+    post = models.ForeignKey(
+        'Post', on_delete=models.CASCADE, related_name='comments')
+
+    # a comment has one author. link a foreignkey to author
+    author = models.ForeignKey(
+        'User', on_delete=models.CASCADE, related_name='comments')
