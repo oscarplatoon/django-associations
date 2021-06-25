7546f7a82046bd41815c06f4f29ba541fd021bee
Won Yang <won@gatech.edu>
2021-06-25 09:13:07 -0500

    adds imdb solution

--- imdb/moviedb/models.py
+++ imdb/moviedb/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
 
 class Actor(models.Model):
     pass
 
+
 class Movie(models.Model):
-    pass
+    actors = models.ManyToManyField(
+        Actor, through='Role', related_name='movies')
+    # through specifies what the intermediate table is
+
+
 class Role(models.Model):
-    pass
+    actor = models.ForeignKey(Actor, on_delete=CASCADE, related_name='roles')
+    movie = models.ForeignKey(Movie, on_delete=CASCADE, related_name='roles')
