d424f6e0e6e85e86932dac335d223dfd86d993b6
Jack Shuff <16825267+JWShuff@users.noreply.github.com>
2021-06-30 15:46:57 -0400

    Rel 2 complete. Amazon/Store folder format were not quite the same as the others so I rewrote with a whole new startproject/startapp in django-admin

--- amazon/store/models.py
+++ amazon/store/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    product = models.ForeignKey('Product',
+        on_delete=models.CASCADE,
+        related_name='reviews')
+    user = models.ForeignKey('User',
+        on_delete=models.CASCADE,
+        related_name='reviews')
+
+class Shop(models.Model):
+    owner = models.OneToOneField('User',
+        on_delete=models.CASCADE,
+        related_name='shop')
+
+class Product(models.Model):
+    shop = models.ForeignKey('Shop',
+        on_delete=models.CASCADE,
+        related_name='products')
+
+class User(models.Model):
+    pass
+
+    
+
+# ```
+# shop.owner # should return a User
+# shop.products # products sold by this shop
+
+# product.shop
+# product.reviews
+
+# review.product
+# review.user
+
+# user.reviewed_products
+# user.shop # if this user owns a shop, returns the shop. For most users this would return nil.
+# ```
