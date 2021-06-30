001f62885b2bfe4bad207d9eac0c6383ba71624e
Jack Shuff <16825267+JWShuff@users.noreply.github.com>
2021-06-29 21:28:38 -0400

    Implemented Won's solution, real important the order of classes here.

--- imdb/moviedb/models.py
+++ imdb/moviedb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    pass
+
+
+class Movie(models.Model):
+
+    actors = models.ManyToManyField(
+        Actor, through='Role', related_name='movies')
+    # through specifies what the intermediate table is
+
+
+class Role(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE, related_name='roles')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='roles')
