cbba79cbbc5d8fc66c256d24e6af70e865c4ebf1
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-07-07 20:19:11 -0500

    first attempt at django associations;  amazon

--- amazon/store/models.py
+++ amazon/store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    pass 
+    
+
+class Shop(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Product(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='products')
+    product_reviews = models.ManyToManyField(User, through='Review')
+
+class Review(models.Model): # this is your join table!!!!!
+    # user is providing review
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    
+    # we are reviewing the product, itself
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    
+    # can also add--
+    text = models.TextField()
